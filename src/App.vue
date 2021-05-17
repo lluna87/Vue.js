b2dea1f55b06618e47872b10a566f3ceb0b9c1de
Luis Luna <33042685+lluna87@users.noreply.github.com>
2021-05-17 00:09:43 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>VueJS</h1>
+  <Car />
+</template>
+
+<script>
+
+import Car from "./components/Car";
+
+export default {
+    components: {
+        Car
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Car.vue
@@ -0,0 +1,53 @@
+<template>
+  <h2>Coche</h2>
+  <p>Marca: {{ brand }}</p>
+  <p>Modelo: {{ model }}</p>
+  <p>Color: {{ color[1] }}</p>
+  <p>Precio: {{ precio }} U$D</p>
+  <p>Potencia: {{ power }} CV</p>
+  <p>{{ mensaje.title }} - {{ mensaje.text }}</p>
+</template>
+
+<script>
+export default {
+    // data() {
+    //     return {
+    //         brand: "Audi",
+    //         model: 'A4',
+    //         color: ["Azul", "Rojo", "Verde"],
+    //         price: 10000,
+    //         power: 180,
+    //         mensaje: {
+    //             title: "Hola Mundo",
+    //             text: "Adiós",
+    //         }
+    //     }
+    // }
+    setup() {
+        const brand = "Audi";
+        const model = "A4";
+        const color = ["Azul", "Rojo", "Verde"];
+        const price = 10000;
+        const power = 180;
+
+        const mensaje = {
+                title: "Hola Mundo",
+                text: "Adiós"
+            };
+
+        return {
+            brand,
+            model,
+            color,
+            price,
+            power,
+            mensaje
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
