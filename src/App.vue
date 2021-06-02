24271dc0a51c2f071f764af45d892628d88598b1
Luis Luna <33042685+lluna87@users.noreply.github.com>
2021-06-02 02:56:09 -0300

    Added examples for event handlers binding

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <h1>VueJS</h1>
   <div>
+    <h2>Car 4: Props</h2>
+    <!-- <Car4 power="power" /> --> <!-- TEXTO -->
+    <Car4 :power="power" :upPower="upPower" @downPower="downPower" />
+  </div>
+  <hr>
+  <div>
     <h2>Car - Options</h2>
     <Car3Options />
   </div>
@@ -28,13 +34,24 @@ import Car from "./components/Car";
 import Car2 from "./components/Car2";
 import Car3Options from "./components/Car3Options";
 import Car3Composition from "./components/Car3Composition";
+import Car4 from "./components/Car4";
+import { ref } from 'vue';
 
 export default {
     components: {
         Car,
         Car2,
         Car3Options,
-        Car3Composition
+        Car3Composition,
+        Car4
+    },
+    setup() {
+      let power = ref(73);
+
+      const upPower = ()  =>  { power.value++; }
+      const downPower = ()  =>  { power.value--; }
+
+      return { power, upPower, downPower };
     }
 }
 </script>
---
+++ src/components/Car4.vue
@@ -0,0 +1,41 @@
+<template>
+  <h3>Coche - Options</h3>
+  <p>Marca: {{ brand }}</p>
+  <p>Modelo: {{ model }}</p>
+  <p>Power: {{ power }}</p>
+  <button @click="upPower">Aumentar</button>
+  <button @click="downPowerFn">Disminuir</button>
+</template>
+
+<script>
+export default {
+    props: {
+        // power: String
+        power: {
+            type: Number,
+            default: 60
+        },
+        upPower: Function,
+        downPower: Function
+    },
+    emits: ["downPower"],
+    setup(props,context) { /*setup(props) {*/
+        const brand = "Audi";
+        const model = "A4";
+
+        const downPowerFn = () => {
+            context.emit("downPower");
+        }
+
+        return {
+            brand,
+            model,
+            downPowerFn
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
